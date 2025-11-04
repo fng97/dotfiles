66c4a3dff77cd9d500a9ad8fc3ba9ffee83702f7
fng97 <git@francisco.wiki>
2025-11-04 18:57:56 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 set -euxo pipefail
 
-mkdir -p "$HOME/.config"/{fish,nvim,wezterm,direnv}
+mkdir -p "$HOME/.config"/{fish,nvim,wezterm,wezterm/colors,direnv}
 
 ln -sf "$PWD/fish/config.fish" "$HOME/.config/fish/config.fish"
 ln -sf "$PWD/wezterm/wezterm.lua" "$HOME/.config/wezterm/wezterm.lua"
-ln -sf "$PWD/wezterm/colors" "$HOME/.config/wezterm/colors"
+ln -sf "$PWD/wezterm/colors/vscode-nvim-light.toml" \
+  "$HOME/.config/wezterm/colors/vscode-nvim-light.toml"
+ln -sf "$PWD/wezterm/colors/vscode-nvim-dark.toml" \
+  "$HOME/.config/wezterm/colors/vscode-nvim-dark.toml"
 ln -sf "$PWD/nvim/init.lua" "$HOME/.config/nvim/init.lua"
 ln -sf "$PWD/direnv/direnvrc" "$HOME/.config/direnv/direnvrc"
 ln -sf "$PWD/Brewfile" "$HOME/.Brewfile"
