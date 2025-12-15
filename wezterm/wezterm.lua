d56caabc6bc28e1c6d61296ba270c781dff392a6
Francisco <git@francisco.wiki>
2025-12-15 16:06:34 +0000

    wezterm: Use Debian and bring back tab navigation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local w = require("wezterm")
 local config = w.config_builder()
 
 config.font = w.font("JetBrains Mono")
-config.font_size = 16
+config.font_size = 15
 config.window_decorations = "INTEGRATED_BUTTONS"
 
 -- Switch between light and dark themes based on system theme.
@@ -17,13 +17,17 @@ end
 if w.target_triple == "x86_64-pc-windows-msvc" and w.running_under_wsl then
 	config.wsl_domains = {
 		{
-			name = "WSL:NixOS",
-			distribution = "NixOS",
-			default_cwd = "/home/fng",
+			name = "WSL:Debian",
+			distribution = "Debian",
+			default_cwd = "/home/fngoncalves",
 		},
 	}
-	config.default_domain = "WSL:NixOS"
-	config.font_size = 12
+	config.default_domain = "WSL:Debian"
 end
+
+config.keys = {
+	{ key = "h", mods = "ALT", action = w.action.ActivateTabRelative(-1) },
+	{ key = "l", mods = "ALT", action = w.action.ActivateTabRelative(1) },
+}
 
 return config
