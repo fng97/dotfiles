60c6fa561b98f11cc3c4d8adf40a9e2660ffd433
Francisco <git@francisco.wiki>
2025-12-16 16:07:05 +0000

    fish: Add Windows-specific variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,16 @@
 set fish_greeting
 
+set --export EDITOR nvim
+set --export VISUAL nvim
+set --export BROWSER open
+set --export HOMEBREW_NO_ANALYTICS 1
+
 if test (uname) = "Darwin"
   eval (/opt/homebrew/bin/brew shellenv)
 end
 
-set -x EDITOR nvim
-set -x VISUAL nvim
-set -x BROWSER open
-set -x HOMEBREW_NO_ANALYTICS 1
+if grep --quiet WSL2 /proc/version
+  set --export BROWSER explorer.exe
+end
 
 direnv hook fish | source
