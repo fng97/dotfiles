976cb376b3f44f797bb175d6b07c1579506ab002
Francisco <git@francisco.wiki>
2025-12-17 02:29:59 +0000

    fish: Use brew on Linux too

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,8 @@ set --export HOMEBREW_NO_ANALYTICS 1
 
 if test (uname) = "Darwin"
   eval (/opt/homebrew/bin/brew shellenv)
+else
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 
 if uname -r | grep --quiet WSL2
