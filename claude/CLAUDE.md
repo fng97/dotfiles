bd2a0e9dbbd5166c647df595afd15b4841ef4894
Francisco Nevitt Gonçalves <git@francisco.wiki>
2026-04-28 15:23:53 +0100

    claude: Don't track claude settings

--- claude/CLAUDE.md
+++
@@ -1,8 +0,0 @@
-# ~/.claude/CLAUDE.md
-
-- Prefer fetching GitHub content with `wget` or `curl` rather than `gh`.
-
-## Zig
-
-- Where a style is not specified, use the [TigerStyle coding
-  conventions][https://github.com/tigerbeetle/tigerbeetle/blob/main/docs/TIGER_STYLE.md].
--- install.sh
+++ install.sh
@@ -3,15 +3,11 @@
 set -euxo pipefail
 
 mkdir -p "$HOME/.config"/{fish,wezterm,nvim,direnv}
-mkdir -p "$HOME/.claude"
 mkdir -p "$HOME/.local/bin"
 ln -sf "$PWD/fish/config.fish" "$HOME/.config/fish/config.fish"
 ln -sf "$PWD/wezterm/wezterm.lua" "$HOME/.config/wezterm/wezterm.lua"
 ln -sf "$PWD/nvim/init.lua" "$HOME/.config/nvim/init.lua"
 ln -sf "$PWD/direnv/direnvrc" "$HOME/.config/direnv/direnvrc"
-ln -sf "$PWD/claude/CLAUDE.md" "$HOME/.claude/CLAUDE.md"
-ln -sf "$PWD/claude/settings.json" "$HOME/.claude/settings.json"
-ln -sf "$PWD/claude/commands" "$HOME/.claude/commands"
 ln -sf "$PWD/feeds.txt" "$HOME/.config/feeds.txt"
 ln -sf "$PWD/Brewfile" "$HOME/.Brewfile"
 ln -sf "$PWD/gitconfig" "$HOME/.gitconfig"
@@ -21,6 +17,6 @@ ln -sf "$PWD/feeds.py" "$HOME/.local/bin/feeds"
 # On Windows, symlinking to Windows home from WSL doesn't work because Wezterm can't access the WSL
 # filesystem. Just overwrite the Windows home config each time.
 if uname -r | grep --quiet WSL2; then
-  # See https://superuser.com/a/1546688 for enabling $USERPROFILE.
-  cp "$PWD/wezterm/wezterm.lua" "$USERPROFILE/.config/wezterm/wezterm.lua"
+    # See https://superuser.com/a/1546688 for enabling $USERPROFILE.
+    cp "$PWD/wezterm/wezterm.lua" "$USERPROFILE/.config/wezterm/wezterm.lua"
 fi
